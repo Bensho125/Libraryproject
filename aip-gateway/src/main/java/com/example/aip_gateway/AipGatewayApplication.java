744c59222f17f0a5218db4a3d6742a0bac049cf2
Bensho125 <112951051+Bensho125@users.noreply.github.com>
2025-11-09 14:31:44 +0200

    core infastructure

---
+++ aip-gateway/src/main/java/com/example/aip_gateway/AipGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.aip_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AipGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AipGatewayApplication.class, args);
+	}
+
+}
---
+++ aip-gateway/src/test/java/com/example/aip_gateway/AipGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aip_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AipGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
